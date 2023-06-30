15776a92c56a196df44bfa15d7a5dd82fee958b7
LoganStalker <dokvor63@mail.ru>
2023-06-30 18:29:26 +0500

    refactor.

--- botapp/handlers/main.py
+++ botapp/handlers/main.py
@@ -4,10 +4,11 @@ import re
 from pyrogram import Client, filters
 from pyrogram.types import (
     ReplyKeyboardMarkup,
-    KeyboardButton, ForceReply
+    KeyboardButton
 )
 
-from .utils import get_token,connect_to_pc
+from .utils import get_token, connect_to_pc
+
 
 @Client.on_message(filters.command("help", ["/", ".", "?"]))
 async def helper(client, message):
